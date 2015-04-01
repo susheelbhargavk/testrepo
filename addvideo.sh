7da12e341c700463c84c078d1b147fd038384b0b
AkhilHector <akhilhotboy@gmail.com>
2015-04-01 18:12:52 +0530

    Update script

--- addvideo.sh
+++ addvideo.sh
@@ -1,7 +1,11 @@
 cd gitdir/Testrepo-1/
-echo "enter the name of the video with extension"
+echo "Enter the name of the video with extension"
 read vid
-git add vid
+git add $vid
 git commit -m "new video added"
 git pull origin master
 git push origin master
+
+echo "Do you want to play again from the start"
+cd gitdor/Testrepo-1/
+vlc * -f
